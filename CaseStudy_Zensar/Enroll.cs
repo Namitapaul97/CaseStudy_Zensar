74cb6f45332ec9fba2b6177c92aedc2b5b4f0a22
NP65847 <NP65847@POOLW42NLPC0634>
2022-03-09 01:10:56 +0530

    Add project files.

---
+++ CaseStudy_Zensar/Enroll.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseStudy_Zensar
+{
+    internal class Enroll
+    {
+        private Student student { get; set; }
+        private Course course { get; set; }
+        private DateTime enrollmentDate { get; set; }
+        public Enroll()
+        {
+            this.student = student;
+            this.course = course;
+            this.enrollmentDate = enrollmentDate;
+        }
+}
+}
